dc453509855f5181f4a7ecd34e904230f3d5bb4a
Anshu Tomar <anshu@Praveens-MBP.fios-router.home>
2018-11-10 13:41:07 -0500

    run.py to run the application

---
+++ run.py
@@ -0,0 +1,21 @@
+"""
+KeplerGroup-CodingExcercise Runner
+
+Start the Service
+"""
+
+import os
+from app import app
+
+# Pull options from environment
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+PORT = os.getenv('PORT', '5000')
+
+######################################################################
+#   M A I N
+######################################################################
+if __name__ == "__main__":
+    print("****************************************")
+    print(" KEPLER GROUP CODING EXCERCISE   SERVICE   RUNNING")
+    print("****************************************")
+    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
