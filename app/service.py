6ac5e56399692e3419e390e451a3d14bd4ef3c8b
Anshu Tomar <anshu@Praveens-MBP.fios-router.home>
2018-11-10 19:33:45 -0500

    comments added

--- app/service.py
+++ app/service.py
@@ -3,11 +3,10 @@ Kepler Group Coding Excercise
 
 Paths:
 -----
-
-
-GET /words/randomword?input=space-seperated-words  - Takes list of words and returns a random word out of them 
-GET /words/rhymingwords/{word} = one - Takes a word and return list of words rhyming with the given word
-
+GET /words/randomword?input=<comma sepearted list of words>  - Takes list of words and returns a random word out of them
+GET /words/rhymingwords/{word} - Takes a word and return list of words rhyming with the given word
+-----
+"""
 
 import os
 import sys
@@ -17,45 +16,95 @@ from flask_api import status
 from werkzeug.exceptions import BadRequest, NotFound, UnsupportedMediaType, InternalServerError # Exception Class
 
 from . import app
-from models import RandomWord, Rhyme, DataValidationError
+from app.models import RandomWord, Rhyme
 
 
+"""
+#####################################################################
+GET INDEX PAGE
+#####################################################################
+"""
+@app.route('/',methods=['GET'])
+def index():
+    """ Root URL response """
+    print("Words REST API Service")
+    return jsonify(name='Words REST API Service',
+                   version='1.0'
+                  ), status.HTTP_200_OK
 
-""""
-
+"""
 ######################################################################
 # GET RANDOM WORD
 ######################################################################
+"""
 @app.route('/words/randomword', methods=['GET'])
 def get_random_word():
     """
-    This endpoint will return a word randomly selected from the given list of 
-    words as parameter
+    This endpoint will return a word randomly selected from the given list of
+    words in the query
 
     """
-    words = request.args['input'].split(" ")
+    print("GET_RANDOM_WORD")
+
+    #Split the input list into words
+    if 'input' in request.args:
+        words = request.args['input'].split(",")
+    else:      # Return detailed error message if the 'input' is not provided
+        return jsonify(status=404,error='Required parameters not found.',\
+                        message='Please specify the input',\
+                        more_info='Use something like /words/randomword?input=word1,word2,word3'), 404
+
+    # Return detailed error message if the number of words is less that two
     if words is None or len(words) < 2:
-        return make_response('', status.HTTP_400_BAD_REQUEST)
+        return jsonify(status=404,error='Required number of parameters not found.',\
+                        message='Please input atleast two or more words',\
+                        more_info='Use something like /words/randomword?input=word1,word2,word3'), 404
 
+    #Call the method from the RandomWord class to get the random word
     word = RandomWord.get_random_word(words)
-    return make_response(jsonify(word.serialize()), status.HTTP_200_OK)
+    return make_response(jsonify(RandomWord.serialize(word)), status.HTTP_200_OK)
 
 
 
+
+"""
 ######################################################################
 # GET RHYMING WORDS LIST
 ######################################################################
+"""
 @app.route('/words/rhymingwords/<string:word>', methods=['GET'])
-def get_rhyming_words(word):
+def get_rhyming_words(word='Kepler'):
     """
-    This endpoint will return list of words rhyming with the given 
-    words as parameter. It will skip the words after the first space
-    if more than one word is given as input. 
-    
+    This endpoint will return list of words rhyming with the given
+    words as parameter. It will skip the words after the first comma
+    if a comma separated list is provided and will return the list of
+    words rhyming with that word.
+
     """
-    input = word.split(" ")[0]
+    print("GET_RHYMING_WORDS")
+
+    #Get the first word if a comma seperated list is provided
+    input = word.split(",")[0]
+
+    #Call the method from the Rhyme class to get the rhyming words list
     rhyming_words = Rhyme.get_rhyming_words(input)
-    result = [rhyming_word.serialize() for rhyming_word in  rhyming_words]
-    return make_response(jsonify(result), status.HTTP_200_OK)
+    return make_response(jsonify(rhyming_words), status.HTTP_200_OK)
 
 
+"""
+######################################################################
+# RHYMING WORDS LIST ROUTE INDEX
+######################################################################
+"""
+@app.route('/words/rhymingwords/', methods=['GET'])
+def get_rhyming_words_empty():
+    """
+    This endpoint will return a response asking the user to provide
+    input word.
+
+    """
+    print("GET_RHYMING_WORDS_EMPTY")
+
+    return jsonify(status=404,error='Input parameter is missing.',\
+                    message='Please provide the word whose rhyming words you want',\
+                    more_info='Use something like /words/randomword/cat'), 404
