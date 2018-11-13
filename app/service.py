ef4e00289bc885a169c5dcf8b4c83f2d9f955829
Anshu Tomar <anshu@Praveens-MBP.fios-router.home>
2018-11-13 11:55:05 -0500

    pig-latin endpoint added

--- app/service.py
+++ app/service.py
@@ -5,6 +5,7 @@ Paths:
 -----
 GET /words/random?input=<comma sepearted list of words>  - Takes list of words and returns a random word out of them
 GET /words/rhyming/{word} - Takes a word and return list of words rhyming with the given word
+GET /words/pig-latin/{word} - Takes a word and returns its pig-latin translation
 -----
 """
 
@@ -16,7 +17,7 @@ from flask_api import status
 from werkzeug.exceptions import BadRequest, NotFound, UnsupportedMediaType, InternalServerError # Exception Class
 
 from . import app
-from app.models import RandomWord, Rhyme
+from app.models import RandomWord, Rhyme, PigLatin
 
 """
 ######################################################################
@@ -26,21 +27,21 @@ from app.models import RandomWord, Rhyme
 
 @app.errorhandler(400)
 def bad_request(error):
-    """ Handles bad reuests with 400_BAD_REQUEST """
-    error = 'Required parameters not found.'
-    message = 'Please specify the input/at least two words in the input.'
-    more_info='Use something like /words/random?input=word1,word2,word3.'
+    """ Handles bad requests with 400_BAD_REQUEST """
+    #error = 'Required parameters not found.'
+    #message = 'Please specify the input/at least two words in the input.'
+    message = str(error)
+    #more_info='Use something like /words/random?input=word1,word2,word3.'
     app.logger.info(error)
     return jsonify(status=400,\
-                   error=error,\
-                   message=message,\
-                   more_info=more_info), 400
+                   error='Bad Request',\
+                   message=message), 400
 
 
 @app.errorhandler(404)
 def not_found(error):
     """ Handles resources not found with 404_NOT_FOUND """
-    message = error.message or str(error)
+    message = str(error)
     app.logger.info(message)
     return jsonify(status=404,\
                    error='Not Found', \
@@ -51,7 +52,7 @@ def not_found(error):
 @app.errorhandler(500)
 def internal_server_error(error):
     """ Handles unexpected server error with 500_SERVER_ERROR """
-    message = error.message or str(error)
+    message = str(error)
     app.logger.info(message)
     return jsonify(status=500,\
                    error='Internal Server Error',\
@@ -92,11 +93,17 @@ def get_random_word():
     if 'input' in request.args:
         words = request.args['input'].split(",")
     else:      # Abort the request with error messgae  if the 'input' is not provided
-        abort(400)
+        abort(400,'Input not provided')
 
     # Abort the request with error message if the number of words is less that two
-    if words is None or len(words) < 2:
-        abort(400)
+    # greater than 20
+    if words is None or len(words) < 2 or len(words) > 20:
+        abort(400,'Length of the list is either less than 2 or greater than 20')
+
+    # Abort the request if length of any word is greater than 45.
+    #Longest word in Oxford dictionary is of length 45.
+    if not valid_length_of_words(words):
+        abort(400,'Length of one or more words is greater than 45')
 
     #Call the method from the RandomWord class to get the random word
     word = RandomWord.get_random_word(words)
@@ -124,9 +131,38 @@ def get_rhyming_words(word='Kepler'):
     #Get the first word if a comma seperated list is provided
     input = word.split(",")[0]
 
+    #Abort if the the word is not a valid English word
+    if valid_english_word(input) is False:
+        abort(400,'The word is not a valid english word')
+
     #Call the method from the Rhyme class to get the rhyming words list
     rhyming_words = Rhyme.get_rhyming_words(input)
     return make_response(jsonify(rhyming_words), status.HTTP_200_OK)
+
+
+"""
+######################################################################
+# GET PIG-LATIN WORD
+######################################################################
+"""
+@app.route('/words/pig-latin/<string:word>', methods=['GET'])
+def get_pig_latin_word(word='Kepler'):
+    """
+    This endpoint will return pig-latin translation of the given word.
+    """
+    app.logger.info("Processing pig-latin word request.")
+
+    #Get the first word if a comma seperated list is provided
+    input = word.split(",")[0]
+
+    #Abort if the the word is not a valid English word
+    if valid_english_word(input) is False:
+        abort(400,'The word is not a valid english word')
+
+    #Call the method from the Rhyme class to get the rhyming words list
+    pig_latin_word = PigLatin.get_pig_latin_word(input)
+    return make_response(jsonify(pig_latin_word), status.HTTP_200_OK)
+
 
 
 """
@@ -151,9 +187,9 @@ def get_rhyming_words_empty():
 
 """
 ######################################################################
-# Utility function
+# Utility functions
 ######################################################################
-"""               
+"""
 
 def initialize_logging(log_level=logging.INFO):
     """ Initialized the default logging to STDOUT """
@@ -177,3 +213,21 @@ def initialize_logging(log_level=logging.INFO):
         app.logger.addHandler(handler)
         app.logger.setLevel(log_level)
         app.logger.info('Logging handler established.')
+
+
+def valid_length_of_words(word_list = []):
+    """Validate if the words in the list have valid lengths"""
+    for w in word_list:
+        if len(w) > 45:
+            return False;
+    # In all other cases, the function returns true
+    return True
+
+
+def valid_english_word(word):
+    """Validate if the word is a English word.
+    The check is on the characters of the word.
+    It must contain just alphabets from a-z or A-Z
+    and nothing else.
+    """
+    return word.isalpha()
