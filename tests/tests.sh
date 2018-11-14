ec26165ecaad13ebc4768b5f2be1967c11ab1406
Anshu Tomar <anshu@Praveens-MBP.fios-router.home>
2018-11-14 10:29:20 -0500

    test metrics added

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,21 +2,35 @@
 #The API calls will be executed with valid and invalid parameters
 # Based on the validity of the API call, the HTTP response will be generated
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-red='\033[0;31m'
-nc='\033[0m'
+bold=`tput bold`
+normal=`tput sgr0`
+red=`tput setaf 1`
 
+#Port number on which application is running
+PORT=5000
+
+#Count of total test cases
+total_test_cases=0
+
+#Count of passed test case
+passed_test_cases=0
 
 #Test the home index of the API
 echo "${bold}T E S T I N G   T H E   A P I   H O M E   I N D E X ${normal}"
 echo "${bold} Expected output:${normal} "
+expected="{\"info\":\"Home page for the API\",\"name\":\"Words REST API Service\",\"version\":\"1.0\"}"
 echo "{\"info\":\"Home page for the API\",\"name\":\"Words REST API Service\",\"version\":\"1.0\"}"
 echo "${bold} Actual output:${normal} "
-curl -H "Accept:application/json" "http://localhost:5000/"
+actual=$(curl -s -H "Accept:application/json" "http://localhost:${PORT}/")
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
 
-echo "\n"
-echo "\n"
+if [ "${expected}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
+
+echo ""
+echo ""
 
 
 #Test the random word generation functionality of the API
@@ -25,84 +39,149 @@ echo "\n"
 #Case 3: When no of words in the list are more than 20
 #Case 3: When correct input is given
 
-echo "${bold}T E S T I N G   T H E   R A N D O M   W O R D   G E N E R A T I O N   F U N C T I O N A L I T Y"
-echo "${red}  Case 1: When no input is given to the api${nc}"
+echo "${bold}T E S T I N G   T H E   R A N D O M   W O R D   G E N E R A T I O N   F U N C T I O N A L I T Y ${normal}"
+echo "${red}  Case 1: When no input is given to the api${normal}"
+
 echo "${bold}Expected output :"
 echo "${normal}{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: Input not provided\",\"status\":400}"
+expected="{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: Input not provided\",\"status\":400}"
 
 echo "${bold}Actual output:${normal}"
-curl -H "Accept:application/json" "http://localhost:5000/words/random"
+actual=$(curl -s -H "Accept:application/json" "http://localhost:${PORT}/words/random")
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
 
-echo "\n"
+if [ "${expected}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
 
-echo "${red}  Case 2: When no of words in the list are less than 2 ${nc}"
+echo ""
+
+echo "${red}  Case 2: When no of words in the list are less than 2 ${normal}"
 echo "${bold}Expected output :"
 echo "${normal}{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: Length of the list is either less than 2 or greater than 20\",\"status\":400}"
+expected="{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: Length of the list is either less than 2 or greater than 20\",\"status\":400}"
 
 echo "${bold}Actual output:${normal}"
-curl -H "Accept:application/json" "http://localhost:5000/words/random?input=one"
+actual=$(curl -s -H "Accept:application/json" "http://localhost:${PORT}/words/random?input=one")
 
-echo "\n"
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
 
-echo "${red}  Case 3: When no of words in the list are more than 20 ${nc}"
+if [ "${expected}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
+
+echo ""
+
+echo "${red}  Case 3: When no of words in the list are more than 20 ${normal}"
 echo "${bold}Expected output :"
 echo "${normal}{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: Length of the list is either less than 2 or greater than 20\",\"status\":400}"
+expected="{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: Length of the list is either less than 2 or greater than 20\",\"status\":400}"
 
 echo "${bold}Actual output:${normal}"
-curl -H "Accept:application/json" "http://localhost:5000/words/random?input=one,two,three,one,two,three,one,two,three,one,two,three,one,two,three,one,two,three,one,two,three"
+actual=$(curl -s -H "Accept:application/json" "http://localhost:${PORT}/words/random?input=one,two,three,one,two,three,one,two,three,one,two,three,one,two,three,one,two,three,one,two,three")
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
 
-echo "\n"
+if [ "${expected}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
 
-echo "${red}  Case 4: When correct input is given ${nc}"
+echo ""
+
+echo "${red}  Case 4: When correct input is given ${normal}"
 echo "${bold}Expected output :${normal}"
 echo "{\"random_word\":\"one\"} ${bold}OR${normal} {\"random_word\":\"two\"} ${bold}OR${normal} {\"random_word\":\"three\"}"
+expected_1="{\"random_word\":\"one\"}"
+expected_2="{\"random_word\":\"two\"}"
+expecter_3="{\"random_word\":\"three\"}"
+
 echo "${bold}Actual output:${normal}"
-curl -H "Accept:application/json" "http://localhost:5000/words/random?input=one,two,three"
+actual=$(curl -s  -H "Accept:application/json" "http://localhost:${PORT}/words/random?input=one,two,three")
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
 
+if [ "${expected_1}" == "${actual}" ] || [ "${expected_2}" == "${actual}" ] || [ "${expected_3}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
 
-echo "\n"
-echo "\n"
+echo ""
+echo ""
 
 #Test the getting rhyming words functionality of the API
 #Case 1: When the word is not a valid english word
 #Case 2: When correct input is provided
 
-echo "${bold}T E S T I N G   T H E   R H Y M I N G   W O R D S   F U N C T I O N A L I T Y"
-echo "${red}  Case 1: When the word is not a valid english word ${nc}"
+echo "${bold}T E S T I N G   T H E   R H Y M I N G   W O R D S   F U N C T I O N A L I T Y ${normal}"
+echo "${red}  Case 1: When the word is not a valid english word ${normal}"
 echo "${bold}Expected output :"
 echo "${normal}{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: The word is not a valid english word\",\"status\":400}"
+expected="{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: The word is not a valid english word\",\"status\":400}"
 
 echo "${bold}Actual output:${normal}"
-curl -H "Accept:application/json" "http://localhost:5000/words/rhyming/1cat"
+actual=$(curl -s -H "Accept:application/json" "http://localhost:${PORT}/words/rhyming/1cat")
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
 
-echo "\n"
+if [ "${expected}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
 
-echo "${red}  Case 2: When correct input is given ("earth" is given as input)${nc}"
+echo ""
+
+echo "${red}  Case 2: When correct input is given ("earth" is given as input)${normal}"
 echo "${bold}Expected output :"
 echo "${normal}[\"berth\",\"birth\",\"dearth\",\"firth\",\"fuerth\",\"furth\",\"gerth\",\"girth\",\"hirth\",\"kerth\",\"kurth\",\"mirth\",\"perth\",\"rebirth\",\"unearth\",\"wentworth\",\"werth\",\"wirth\",\"worth\",\"wurth\"]"
-echo "${bold}Actual output:${normal}"
-curl -H "Accept:application/json" "http://localhost:5000/words/rhyming/earth"
+expected="[\"berth\",\"birth\",\"dearth\",\"firth\",\"fuerth\",\"furth\",\"gerth\",\"girth\",\"hirth\",\"kerth\",\"kurth\",\"mirth\",\"perth\",\"rebirth\",\"unearth\",\"wentworth\",\"werth\",\"wirth\",\"worth\",\"wurth\"]"
 
-echo "\n"
-echo "\n"
+echo "${bold}Actual output:${normal}"
+actual=$(curl -s -H "Accept:application/json" "http://localhost:${PORT}/words/rhyming/earth")
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
+
+if [ "${expected}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
+
+echo ""
+echo ""
 
 #Test the pig-latin word translation  functionality of the API
 #Case 1: When the word is not a valid english word
 #Case 2: When correct input is given
 
-echo "${bold}T E S T I N G   T H E   P I G   L A T I N   T R A N S L A T I O N     F U N C T I O N A L I T Y"
-echo "${red}  Case 1: When the word is not a valid english word ${nc}"
+echo "${bold}T E S T I N G   T H E   P I G   L A T I N   T R A N S L A T I O N     F U N C T I O N A L I T Y ${normal}"
+echo "${red}  Case 1: When the word is not a valid english word ${normal}"
 echo "${bold}Expected output :"
-echo "${normal}{\"error\":\"Bad Request\",\"message":\"400 Bad Request: The word is not a valid english word\",\"status\":400}" "
+echo "${normal}{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: The word is not a valid english word\",\"status\":400}"
+expected="{\"error\":\"Bad Request\",\"message\":\"400 Bad Request: The word is not a valid english word\",\"status\":400}"
+echo "${bold}Actual output:${normal}"
+actual=$(curl -s -H "Accept:application/json" "http://localhost:${PORT}/words/pig-latin/2who@")
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
+
+if [ "${expected}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
+
+echo ""
+
+echo "${red}  Case 2: When correct input is given (who is given as input)${normal}"
+echo "${bold}Expected output :"
+echo "${normal}{\"pig_latin_word\":\"howay\"}"
+expected="{\"pig_latin_word\":\"howay\"}"
 
 echo "${bold}Actual output:${normal}"
-curl -H "Accept:application/json" "http://localhost:5000/words/pig-latin/2who@"
+actual=$(curl -s -H "Accept:application/json" "http://localhost:${PORT}/words/pig-latin/who")
+echo "${actual}"
+total_test_cases=$((total_test_cases+1))
 
-echo "\n"
+if [ "${expected}" == "${actual}" ]; then
+	passed_test_cases=$((passed_test_cases+1))
+fi
 
-echo "${red}  Case 2: When correct input is given ("who" is given as input)${nc}"
-echo "${bold}Expected output :"
-echo "${normal}{\"pig_latin_word\":\"howay\"} "
+echo ""
+echo ""
 
-echo "${bold}Actual output:${normal}"
-curl -H "Accept:application/json" "http://localhost:5000/words/pig-latin/who"
+echo "Passed tests/Total tests = ${passed_test_cases}/${total_test_cases}"
