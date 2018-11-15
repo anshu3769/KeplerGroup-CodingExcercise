8a5423109be890760b4150bce9ae964a37af556d
Anshu Tomar <anshu@Praveens-MBP.fios-router.home>
2018-11-15 11:18:47 -0500

    documentattion added

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
-"""
-Kepler Group Coding Excercise
+"""Kepler Group Coding Excercise
 
-Start the Service
+Application starts running from this module.
+It also initializes logging for the application.
+
+As a user, you will be running "python3 run.py"
+to start the application.
 """
 
 import os
 from app import app,service
 
-# Pull options from environment
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+#Pull options from environment, if provided, otherwise default will be chosen
+#DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 PORT = os.getenv('PORT', '5000')
 
 ######################################################################
@@ -19,5 +22,9 @@ if __name__ == "__main__":
     print("KEPLER GROUP CODING EXCERCISE")
     print("SERVICE IS  RUNNING")
     print("****************************************")
+  
+    #Initialize logging
     service.initialize_logging()
-    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
+
+    #Run the application 
+    app.run(host='0.0.0.0', port=int(PORT))
